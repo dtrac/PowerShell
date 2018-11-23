aa0fb45cf6c4b72e8f04950c693f7b80f2891f6f
Dan Tracey <dtracey@hotmail.co.uk>
2018-11-23 15:19:17 +0000

    Update and rename Test-Thing.ps1 to os scripts/Test-Thing.ps1

---
+++ os scripts/Test-Thing.ps1
@@ -0,0 +1,22 @@
+
+$timeout = new-timespan -Seconds 120
+$sw = [diagnostics.stopwatch]::StartNew()
+while (($sw.elapsed -lt $timeout) -and ($success -ne $true)) {
+	try {
+		   Test-Thing -ErrorAction Stop
+		   Write-Verbose -Message "Thing happened"
+		   $success = $true
+	} 
+catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] { # Specific error - $Error[0].Exception.GetType().FullName
+   Write-Warning -Message "Thing didn't happen in $([math]::Round($sw.elapsed.TotalSeconds)) seconds."
+   start-sleep -seconds 5
+}
+finally {
+	if ($success){
+	    Write-Verbose -Message "Success!"
+	}
+	else {
+	    Write-Verbose -Message "Failure!"
+	}
+ }
+}
